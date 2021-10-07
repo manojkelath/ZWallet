4dd8e0d7256e73a5022fa161e5ae9dfb520c8a5a
Manoj Kelath <manojkelath1987@gmail.com>
2021-10-07 14:12:56 -0400

    added new address to Zwallet

--- migrations/2_zwallet_migration.js
+++ migrations/2_zwallet_migration.js
@@ -1,6 +1,6 @@
 const Migrations = artifacts.require("Zwallet");
 
-let owners = ['0xb8eE0623b5d1d072866c58eAE216A41e534C4a87', '0xB29E1c4Cb1D04DaaE96c4970780535AFC9B6Efd6'];
+let owners = ['0xb8ee0623b5d1d072866c58eae216a41e534c4a87', '0xb29e1c4cb1d04daae96c4970780535afc9b6efd6'];
 let limit = 3;
 
 module.exports = function (deployer) {
